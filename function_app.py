4513a1db801355970e6ce7a87ba77fdb662e93d0
Arun Kulkarni <akulkar27@gmail.com>
2023-11-14 12:32:50 -0500

    update function code

--- function_app.py
+++ function_app.py
@@ -1,25 +1,38 @@
 import azure.functions as func
 import logging
+import os
+from azure.data.tables import TableServiceClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+# Initialize Table Service Client
+connection_string = os.environ["COSMOS_DB_CONNECTION_STRING"]
+table_service = TableServiceClient.from_connection_string(conn_str=connection_string)
+table_name = "VisitorCount"  # Replace with your table name
+table_client = table_service.get_table_client(table_name)
+
+def get_and_update_visitor_count():
+    # Try to fetch the existing count
+    try:
+        entity = table_client.get_entity(partition_key="visitorCounter", row_key="0")
+        current_count = entity['count']
+    except Exception as e:
+        logging.error(f"Error fetching entity: {str(e)}")
+        # If the entity doesn't exist, start with count = 0
+        current_count = 0
+
+    # Update the count
+    updated_count = current_count + 1
+    table_client.upsert_entity({"PartitionKey": "VisitorCounter", "RowKey": "Count", "count": updated_count})
+
+    return updated_count
 
 @app.route(route="update_visitor_count")
 def update_visitor_count(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    try:
+        visitor_count = get_and_update_visitor_count()
+        return func.HttpResponse(f"Visitor count updated. Current count: {visitor_count}", status_code=200)
+    except Exception as e:
+        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
